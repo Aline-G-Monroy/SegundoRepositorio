7fb08d93be793f15c16fb3e865b4103fcb88f53d
MrSpecter07 <bootsrecillas@gmail.com>
2024-04-09 20:02:17 -0600

    No le sabes

--- Raúl.java
+++ Raúl.java
@@ -1,8 +1,10 @@
 public class Raúl{
 	public static void main(String[] args){
 		System.out.println("Hola Raúl");
+		int veces = 5;
+		for (int aña = 0, aña<11, aña++){
+			System.out.printf("%0"+veces+"d\n","");
+			System.out.printf("%0"+aña+"d\n",*);
+			veces--;
 	}
 }
-
-
-
